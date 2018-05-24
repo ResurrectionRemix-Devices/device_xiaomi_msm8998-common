70f5df533a65dcbb433f18bb504a1aef54955538
martinusbe <martinusbe@gmail.com>
2018-05-24 16:57:39 +0300

    msm8998: Add wificountrycode=US to kernel command line
    * Fixes 5GHz wireless hotspot

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -51,6 +51,7 @@ BOARD_KERNEL_CMDLINE += user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048
+BOARD_KERNEL_CMDLINE += androidboot.wificountrycode=US
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8998
